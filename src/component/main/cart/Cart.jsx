4491235e683987984c540cc16c4ddbf3991240d8
whps40333 <whps40333@gmail.com>
2022-11-16 17:56:34 +0800

    Initialize Project

--- src/component/main/cart/Cart.jsx
+++ src/component/main/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 import styles from './Cart.module.scss';
 import ProductList from './cartItems/ProductList';
 import PriceList from './cartItems/PriceList';
@@ -21,28 +21,74 @@ const PRODUCTS = [
 ]
 
 
-export default function Cart() {
+export default function Cart({ checkedData }) {
+    const [products, setProducts] = useState(PRODUCTS)
+    function handleQuantitiyAdd(productID) {
+        setProducts(
+            products.map(p => {
+                if (p.id === productID) {
+                    return {
+                        ...p,
+                        quantity: p.quantity + 1,
+                    }
+                } else {
+                    return p
+                }
+            })
+        )
+    }
+    function handleQuantitiyMinus(productId) {
+        let newProducts = products.map(p => {
+            if (p.id === productId && p.quantity > 0) {
+                return {
+                    ...p,
+                    quantity: p.quantity - 1,
+                }
+            } else {
+                return p
+            }
+        })
+        setProducts(newProducts.filter(p => p.quantity > 0))
+    }
     return (
-        <section className={styles.cartContainer}>
-            <h3 className={styles.cartTitle}>購物籃</h3>
-            <section className='productList' data-total-price='0'>
-                {PRODUCTS.map(product => {
+        <section className={styles.cart__container}>
+            <h3 className={styles.cart__title}>購物籃</h3>
+            <section className='product__list' data-total-price='0'>
+                {products.map(product => {
                     return (
                         <ProductList
                             key={product.id}
-                            dataPrice={product.price}
                             imgSrc={product.img}
+                            productId={product.id}
                             productName={product.name}
                             productPrice={product.price}
                             productQuantity={product.quantity}
+                            handleClickAdd={handleQuantitiyAdd}
+                            handleClickMinus={handleQuantitiyMinus}
                         />
                     )
                 })}
             </section>
-            {/* price will render from state  */}
-            <PriceList text='運費' price='0' />
-            <PriceList text='小計' price='0' />
-
+            {/* price will render from state */}
+            <PriceList
+                text='運費'
+                price={checkedData === 0 ? '免費' : `$${checkedData}`}
+            />
+            {/* <PriceList text='小計' price='0' /> */}
+            <CalculatePrice products={products} shippingPrice={checkedData} />
         </section>
     )
 }
+function CalculatePrice({ products, shippingPrice }) {
+    let totalPrice = null
+    products.map(p => (totalPrice = totalPrice + p.price * p.quantity))
+    totalPrice += shippingPrice
+    return (
+        <PriceList
+            text='小計'
+            price={
+                products.length > 0 ? `$${totalPrice.toLocaleString()}` : '購物籃是空的'
+            }
+        />
+    )
+}      
